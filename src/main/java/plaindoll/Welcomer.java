8f66ec22326a7b65b959b672ad1e97d1c03c433d
Skeltis <Morfenume@yandex.ru>
2026-01-20 20:34:49 +0300

    [TASK-3] Hire bounty hunter

--- src/main/java/plaindoll/Welcomer.java
+++ src/main/java/plaindoll/Welcomer.java
@@ -13,4 +13,7 @@ public class Welcomer{
 	public String saySome(){
 		return "something in the way";
 	}
+    public String hireBobaFett() {
+        return "Bounty hunter: He's no good to me dead.";
+    }
 }
